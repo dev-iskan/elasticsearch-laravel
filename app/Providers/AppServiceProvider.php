c3d368cc3def2a3d7c640d781580bc1206b3f5a1
dev-iskan <successfuliskan@gmail.com>
2019-03-22 22:12:43 +0500

    created own search engine, added users to index

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Search\Engines\ElasticSearchEngine;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Scout\EngineManager;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        resolve(EngineManager::class)->extend('elasticsearch', function () {
+            return new ElasticSearchEngine();
+        });
     }
 
     /**
---
+++ app/Search/Engines/ElasticSearchEngine.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Search\Engines;
+
+use Laravel\Scout\Builder;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Scout\Engines\Engine;
+
+class ElasticSearchEngine extends Engine
+{
+    /**
+     * Update the given model in the index.
+     *
+     * @param  \Illuminate\Database\Eloquent\Collection  $models
+     * @return void
+     */
+     public function update($models){
+         //take collection of models
+     }
+
+    /**
+     * Remove the given model from the index.
+     *
+     * @param  \Illuminate\Database\Eloquent\Collection  $models
+     * @return void
+     */
+     public function delete($models){
+         //delete from index
+     }
+
+    /**
+     * Perform the given search on the engine.
+     *
+     * @param  \Laravel\Scout\Builder  $builder
+     * @return mixed
+     */
+     public function search(Builder $builder){
+        //don't  return result
+     }
+
+    /**
+     * Perform the given search on the engine.
+     *
+     * @param  \Laravel\Scout\Builder  $builder
+     * @param  int  $perPage
+     * @param  int  $page
+     * @return mixed
+     */
+     public function paginate(Builder $builder, $perPage, $page){}
+
+    /**
+     * Pluck and return the primary keys of the given results.
+     *
+     * @param  mixed  $results
+     * @return \Illuminate\Support\Collection
+     */
+     public function mapIds($results){}
+
+    /**
+     * Map the given results to instances of the given model.
+     *
+     * @param  \Laravel\Scout\Builder  $builder
+     * @param  mixed  $results
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+     public function map(Builder $builder, $results, $model){
+         // return result from elasticsearch
+     }
+
+    /**
+     * Get the total count from a raw result returned by the engine.
+     *
+     * @param  mixed  $results
+     * @return int
+     */
+     public function getTotalCount($results){}
+
+    /**
+     * Flush all of the model's records from the engine.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+     public function flush($model){}
+}
